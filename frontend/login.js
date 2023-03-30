ca02607d9cd13c90c123608313209dd82a8129a5
LUCA LENCIONI <luca.lencioni09@gmail.com>
2023-03-30 20:45:40 +0200

    Changed Login Logic

--- frontend/login.js
+++ frontend/login.js
@@ -13,23 +13,24 @@ validateLogin(await APIService.post(JSON.stringify(authData), "/login"), authDat
 /**
  * Check if the login data provided is valid
  * 
- * @param {String} valid API response at POST /login {data}
+ * @param {String} uuid API response at POST /login {data}
  * @param {Object} username {nameInput:"Luciano",password:"changeme"}
  * @param {String} remember if "on" memorize in localStorage instead of session storage
  */
-function validateLogin(valid, username, remember) {
-    if (valid !== "valid") {
+function validateLogin(uuid, username, remember) {
+    if (uuid === "invalid") {
         showSnackbarRedText("Login Information Invalid");
         return false;
     } else {
         if (remember !== "on") {
             sessionStorage.setItem('username', username)
+            sessionStorage.setItem('uuid', uuid)
         } else {
             localStorage.setItem('username', username)
+            localStorage.setItem('uuid', uuid)
         }
         window.open('/homepage.html', "_self")
     }
-
 }
 
 

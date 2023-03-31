d9b1616ec37d412849ce2707dca877bd36cf9305
Cabumy <129043670+Cabumy@users.noreply.github.com>
2023-03-31 21:35:59 +0200

    see if the JsonObject is an user or email

--- backend/src/JSONParse.java
+++ backend/src/JSONParse.java
@@ -5,4 +5,37 @@ public class JSONParse {
     public static JSONObject parseStringToJson(String jsonString) throws ParseException {
         return (JSONObject) new JSONParser().parse(jsonString);
     }
+    public boolean ViewEmailOrUsername(JSONObject object,Statement stats)
+    {
+        Final String invalid="ivalid";
+        boolean verifica=false;
+        String nameInput = (String)object.get("nameInput");
+        String password = (String)object.get("password");
+        char[] chars = new char[nameInput.length()];
+        for(int c=0;chars.length();c++)
+        ResultSet rs1 = stats.executeQuery("Select * From Users where Username="+"'"+nameInput+"'"+" and "+"password="+"'"+password+"'");
+        {
+            if(chars[c]==@);
+            while(rs1.next()){
+                String pas = rs1.getString("password");
+                if(pas==password)
+                {
+                    verifica=true;
+                }
+            }
+        }else {
+            while(rs1.next()){
+                String use = rs1.getString("Username");
+                if(use==nameInput)
+                {
+                    verifica=true;
+                }
+            }
+        }
+        if(verifica==true)
+        {
+            return rs1.getString("UserUUID");
+
+        }else return invalid;
+    } 
 }
